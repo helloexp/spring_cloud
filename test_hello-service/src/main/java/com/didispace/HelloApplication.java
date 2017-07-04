03b987fa9a46d8ebaabc03e09ab86be0b1c7181d
qmh <minghui.qiu@hand-china.com>
2017-07-04 11:11:42 +0800

    update

--- test_hello-service/src/main/java/com/didispace/HelloApplication.java
+++ test_hello-service/src/main/java/com/didispace/HelloApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 //@EnableHystrix
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloApplication {
 
